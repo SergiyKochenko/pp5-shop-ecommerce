b6539e0e93c55bec78db2c96e3dda7072d409d07
SergiyKochenko <sergiykochenko@gmail.com>
2023-04-16 02:35:34 +0000

    added custom siginup form

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -121,15 +121,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-# ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
-# -----------------------
-ACCOUNT_EMAIL_VERIFICATION = "none"
-ACCOUNT_LOGOUT_ON_GET = True
-# ------------------------
+
 
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
